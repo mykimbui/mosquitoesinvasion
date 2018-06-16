8089247f90b64b08d202da25f714db7939f60f6b
My Kim Bui <mykimbuibui@gmail.com>
2018-06-16 19:12:57 +0200

    hide

--- js/disappear.js
+++ js/disappear.js
@@ -1,27 +1,27 @@
-// document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(event) {
 
-//   var mosquitoes = document.querySelectorAll('.mosquito');
+  var mosquitoes = document.querySelectorAll('.mosquito');
 
-//   mosquitoes.forEach(function(element) {
-//     element.addEventListener('click', function() {
-//       this.classList.add("hidden");
-//     });
-//   });
-
-// });
-
-$(document).ready(function() {
-
-  // var total = $('ul li:visible').length;
-  $('ul li').each(function() {
-    $(this).click(function() {
-      $(this).hide();
+  mosquitoes.forEach(function(element) {
+    element.addEventListener('click', function() {
+      this.classList.add("hidden");
     });
   });
 
-  if ( $('ul li:visible').length == 0) {
-    $('#win').removeClass('hidden');
-    window.location = "http://against-malaria.pagedemo.co/";
-  }
-
 });
+
+// $(document).ready(function() {
+
+//   // var total = $('ul li:visible').length;
+//   $('ul li').each(function() {
+//     $(this).click(function() {
+//       $(this).hide();
+//     });
+//   });
+
+//   if ( $('ul li:visible').length == 0) {
+//     $('#win').removeClass('hidden');
+//     window.location = "http://against-malaria.pagedemo.co/";
+//   }
+
+// });
